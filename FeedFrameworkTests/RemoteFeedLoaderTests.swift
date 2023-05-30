54dead38ae0ed0eec699a6f6d37a5d702f1806f7
jmtome <jtome@fi.uba.ar>
2023-05-30 16:11:59 -0300

    Make the http client a protocol

--- FeedFrameworkTests/RemoteFeedLoaderTests.swift
+++ FeedFrameworkTests/RemoteFeedLoaderTests.swift
@@ -8,20 +8,23 @@
 import XCTest
 
 class RemoteFeedLoader {
+    let client: HTTPClient
+    
+    init(client: HTTPClient) {
+        self.client = client
+    }
     func load() {
-        HTTPClient.shared.get(from: URL(string: "http://a-url.com")!)
+        client.get(from: URL(string: "http://a-url.com")!)
     }
 }
-class HTTPClient {
-    static var shared = HTTPClient()
-        
-    func get(from url: URL) {}
+protocol HTTPClient {
+    func get(from url: URL) 
 }
 
 class HTTPClientSpy: HTTPClient {
     var requestedURL: URL?
     
-    override func get(from url: URL) {
+    func get(from url: URL) {
         requestedURL =  url
     }
 }
@@ -30,8 +33,7 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     func test_init_doesNotRequestDataFromURL() {
         let client = HTTPClientSpy()
-        HTTPClient.shared = client
-        _ = RemoteFeedLoader()
+        _ = RemoteFeedLoader(client: client)
         
         
         XCTAssertNil(client.requestedURL)
@@ -40,8 +42,7 @@ class RemoteFeedLoaderTests: XCTestCase {
     func tests_load_requestDataFromURL() {
         //MARK: - Given (a client and a sut)
         let client = HTTPClientSpy()
-        HTTPClient.shared = client
-        let sut = RemoteFeedLoader()
+        let sut = RemoteFeedLoader(client: client)
         
         //MARK: - When (we invoke sut.load())
         sut.load()
