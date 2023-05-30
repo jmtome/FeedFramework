d581375510f1136a6beef4dc3cea14499d046bda
jmtome <jtome@fi.uba.ar>
2023-05-30 15:24:35 -0300

    Create FeedFramework and create feed item module

---
+++ FeedFramework/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  FeedFramework
+//
+//  Created by macbook on 30/05/2023.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
