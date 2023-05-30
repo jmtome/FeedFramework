ddf923ba00f98fc1e76592f26823c5d5432b0a58
jmtome <jtome@fi.uba.ar>
2023-05-30 15:21:24 -0300

    Initial Commit

---
+++ FeedFramework/FeedFramework.h
@@ -0,0 +1,18 @@
+//
+//  FeedFramework.h
+//  FeedFramework
+//
+//  Created by macbook on 30/05/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FeedFramework.
+FOUNDATION_EXPORT double FeedFrameworkVersionNumber;
+
+//! Project version string for FeedFramework.
+FOUNDATION_EXPORT const unsigned char FeedFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FeedFramework/PublicHeader.h>
+
+
