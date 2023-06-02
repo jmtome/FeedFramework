328c7473b2758f71d4045ea7d6bb49b61a474fd5
jmtome <jtome@fi.uba.ar>
2023-06-02 17:00:44 -0300

    Move URL creation to a shared factory method

--- FeedFrameworkTests/Feed API/URLSessionHTTPClientTests.swift
+++ FeedFrameworkTests/Feed API/URLSessionHTTPClientTests.swift
@@ -49,7 +49,7 @@ class URLSessionHTTPClientTests: XCTestCase {
     
     func test_getFromURL_performsGETRequestWithURL() {
         
-        let url = URL(string: "http://any-url.com")!
+        let url = anyURL()
         let exp = expectation(description: "Wait for request")
         
         URLProtocolStub.observeRequests { request in
@@ -64,13 +64,12 @@ class URLSessionHTTPClientTests: XCTestCase {
         
     }
     func test_getFromURL_failsOnRequestError() {
-        let url = URL(string: "http://any-url.com")!
         let error = NSError(domain: "any error", code: 1)
-        URLProtocolStub.stub(url: url, data: nil, response: nil, error: error)
+        URLProtocolStub.stub(data: nil, response: nil, error: error)
         
         let exp = expectation(description: "wait for completion")
         
-        makeSUT().get(from: url) { result in
+        makeSUT().get(from: anyURL()) { result in
              switch result {
             case .failure(let receivedError as NSError):
                  XCTAssertEqual(receivedError.domain, error.domain)
@@ -93,6 +92,10 @@ class URLSessionHTTPClientTests: XCTestCase {
         return sut
     }
     
+    private func anyURL() -> URL {
+        return URL(string: "http://any-url.com")!
+    }
+    
     private class URLProtocolStub: URLProtocol {
         private static var stub: Stub?
         private static var requestObserver: ((URLRequest) -> Void)?
@@ -102,7 +105,7 @@ class URLSessionHTTPClientTests: XCTestCase {
             let response: URLResponse?
             let error: Error?
         }
-        static func stub(url: URL, data: Data?, response: URLResponse?, error: Error?) {
+        static func stub(data: Data?, response: URLResponse?, error: Error?) {
             stub = Stub(data: data, response: response, error: error)
         }
         
