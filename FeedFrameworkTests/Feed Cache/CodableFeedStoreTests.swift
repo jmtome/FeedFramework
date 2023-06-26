7b0025f359ee13c98646d5ab704662648974d7ee
jmtome <jtome@fi.uba.ar>
2023-06-26 15:44:37 -0300

    Extract duplicate insert test code into a reusable helper method

--- FeedFrameworkTests/Feed Cache/CodableFeedStoreTests.swift
+++ FeedFrameworkTests/Feed Cache/CodableFeedStoreTests.swift
@@ -100,13 +100,8 @@ final class CodableFeedStoreTests: XCTestCase {
         let feed = uniqueImageFeed().local
         let timestamp = Date()
 
-        let exp = expectation(description: "Wait for cache retrieval")
-
-        sut.insert(feed, timestamp: timestamp) { insertionError in
-            XCTAssertNil(insertionError, "Expected feed to be inserted successfully")
-            exp.fulfill()
-        }
-        wait(for: [exp], timeout: 1.0)
+        insert((feed, timestamp), to: sut)
+        
         expect(sut, toRetrieve: .found(feed: feed, timestamp: timestamp))
     }
     
@@ -114,14 +109,8 @@ final class CodableFeedStoreTests: XCTestCase {
         let sut = createSUT()
         let feed = uniqueImageFeed().local
         let timestamp = Date()
-
-        let exp = expectation(description: "Wait for cache insertion")
-
-        sut.insert(feed, timestamp: timestamp) { insertionError in
-            XCTAssertNil(insertionError, "Expected feed to be inserted successfully")
-            exp.fulfill()
-        }
-        wait(for: [exp], timeout: 1.0)
+        
+        insert((feed, timestamp), to: sut)
         
         expect(sut, toRetrieveTwice: .found(feed: feed, timestamp: timestamp))
     }
@@ -150,6 +139,15 @@ final class CodableFeedStoreTests: XCTestCase {
             exp.fulfill()
         }
         
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    private func insert(_ cache: (feed: [LocalFeedImage], timestamp: Date), to sut: CodableFeedStore) {
+        let exp = expectation(description: "Wait for cache insertion")
+        sut.insert(cache.feed, timestamp: cache.timestamp) { insertionError in
+            XCTAssertNil(insertionError, "Expected feed to be inserted successfully")
+            exp.fulfill()
+        }
         wait(for: [exp], timeout: 1.0)
     }
     
