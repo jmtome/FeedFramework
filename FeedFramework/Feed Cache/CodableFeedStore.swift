395161be0bc0f3838bf6fe24c6b538733a7545f3
jmtome <jtome@fi.uba.ar>
2023-06-26 19:41:50 -0300

    Move CodableFeedStore to its own file in production

---
+++ FeedFramework/Feed Cache/CodableFeedStore.swift
@@ -0,0 +1,91 @@
+//
+//  CodableFeedStore.swift
+//  FeedFramework
+//
+//  Created by macbook on 26/06/2023.
+//
+
+import Foundation
+
+public class CodableFeedStore: FeedStore {
+    private struct Cache: Codable {
+        let feed: [CodableFeedImage]
+        let timestamp: Date
+        
+        var localFeed: [LocalFeedImage] {
+            return feed.map { $0.local }
+        }
+    }
+
+    //We create this private mirror type of LocalFeedImage because we do not want to couple the original model with the Codable conformance,
+    //That would not be ideal because looking at the dependency diagram, the CodableFeedStore would be implementing <FeedStore> which depends on LocalFeedImage
+    //and the CodableFeedStore itself depending on LocalFeedImage
+    //but at the same time we would have LocalFeedImage conforming/depending on Codable which is infrastructure(framework specific) layer, with an arrow returning, and we
+    //don't want that. Because in a future we might want to change our Store into perhaps CoreData/Realm (for which we would'nt need Codable) and whoever comes next might
+    //understand its o.k to keep coupling the model with the infrastructure/framework details. But ideally our LocalFeedImage should be a framework-agnostic type.
+    //
+    //Therefore we create a private local mirror type CodableFeedImage
+    private struct CodableFeedImage: Codable {
+        private let id: UUID
+        private let description: String?
+        private let location: String?
+        private let url: URL
+        
+        init(_ image: LocalFeedImage) {
+            id = image.id
+            description = image.description
+            location = image.location
+            url = image.url
+        }
+        
+        var local: LocalFeedImage {
+            return LocalFeedImage(id: id, description: description, location: location, url: url)
+        }
+    }
+    
+    private let storeURL: URL
+
+    public init(storeURL: URL) {
+        self.storeURL = storeURL
+    }
+    
+    public func retrieve(completion: @escaping RetrievalCompletion) {
+        guard let data = try? Data(contentsOf: storeURL) else {
+          return completion(.empty)
+        }
+        do {
+            let decoder = JSONDecoder()
+            let cache = try decoder.decode(Cache.self, from: data)
+            completion(.found(feed: cache.localFeed, timestamp: cache.timestamp))
+        } catch {
+            completion(.failure(error))
+        }
+    }
+    
+    public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+        do {
+            let encoder = JSONEncoder()
+            let cache = Cache(feed: feed.map(CodableFeedImage.init), timestamp: timestamp)
+            let encoded = try encoder.encode(cache)
+            try encoded.write(to: storeURL)
+            completion(nil)
+        } catch {
+            completion(error)
+        }
+    }
+    
+    public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+        guard FileManager.default.fileExists(atPath: storeURL.path) else {
+            return completion(nil)
+        }
+            
+        do {
+            try FileManager.default.removeItem(at: storeURL)
+            completion(nil)
+        } catch {
+            completion(error)
+        }
+    }
+    
+    
+}
