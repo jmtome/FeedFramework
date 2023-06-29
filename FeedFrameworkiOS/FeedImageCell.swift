ff694e1490f00832b7491f1a71c7e7ccfc26cc8a
jmtome <jtome@fi.uba.ar>
2023-06-29 14:48:26 -0300

    Render loaded feed on successful load completion

---
+++ FeedFrameworkiOS/FeedImageCell.swift
@@ -0,0 +1,15 @@
+//
+//  FeedImageCell.swift
+//  FeedFrameworkiOS
+//
+//  Created by macbook on 29/06/2023.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+    
+}
