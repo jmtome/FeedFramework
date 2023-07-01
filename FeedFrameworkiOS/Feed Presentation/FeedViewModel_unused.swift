cd797633c79083798abcbb2eaf6a81336a0ed33d
jmtome <jtome@fi.uba.ar>
2023-07-01 17:41:07 -0300

    Commented unused FeedViewModel, replaced by FeedPresenter

---
+++ FeedFrameworkiOS/Feed Presentation/FeedViewModel_unused.swift
@@ -0,0 +1,32 @@
+//
+//  FeedViewModel.swift
+//  FeedFrameworkiOS
+//
+//  Created by macbook on 30/06/2023.
+//
+
+import FeedFramework
+
+//MARK: - Unneeded, since we implemented a Presenter
+//final class FeedViewModel {
+//    typealias Observer<T> = (T) -> Void
+//    
+//    private let feedLoader: FeedLoader
+//    
+//    init(feedLoader: FeedLoader) {
+//        self.feedLoader = feedLoader
+//    }
+//    
+//    var onLoadingStateChange: Observer<Bool>?
+//    var onFeedLoad: Observer<[FeedImage]>?
+//    
+//    func loadFeed() {
+//        onLoadingStateChange?(true)
+//        feedLoader.load { [weak self] result in
+//            if let feed = try? result.get() {
+//                self?.onFeedLoad?(feed)
+//            }
+//            self?.onLoadingStateChange?(false)
+//        }
+//    }
+//}
