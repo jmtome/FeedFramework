a2504b8cd0ec6f7ddc717b32ef8762bf5b9a96eb
jmtome <jtome@fi.uba.ar>
2023-07-01 17:38:53 -0300

    Move memory management to Composer Layer. The FeedPresenter shouldnt have to know or handle their dependencies lifetime.

--- FeedFrameworkiOS/Feed UI/Composers/FeedUIComposer.swift
+++ FeedFrameworkiOS/Feed UI/Composers/FeedUIComposer.swift
@@ -16,12 +16,33 @@ public final class FeedUIComposer {
         let refreshController = FeedRefreshViewController(presenter: presenter)
         let feedController = FeedViewController(refreshController: refreshController)
         
-        presenter.loadingView = refreshController
+        presenter.loadingView = WeakRefVirtualProxy(refreshController)
         presenter.feedView = FeedViewAdapter(controller: feedController, imageLoader: imageLoader)
         return feedController
     }
 }
 
+//Memory management is a responsability that belongs in the composer not in your components
+//otherwise you'll be leaking infrastructure details into your MVP components
+
+//So we make a WeakRefVirtualProxy that will hold a weak reference to the object instance and pass the messages forward.
+
+private final class WeakRefVirtualProxy<T: AnyObject> {
+    private weak var object: T?
+    
+    init(_ object: T) {
+        self.object = object
+    }
+}
+//so when we set the LoadView we weakify it with the virutal proxy. And for that to work the proxy should also conform to the FeedLoadingView protocol, which
+//we can do in an extension of the VirtualProxy, we constrain the conformance where the object type must also implement the FeedLoadingView protocol, this way we
+//can safely forward the messages to the weak instance with compile check guarantees
+
+extension WeakRefVirtualProxy: FeedLoadingView where T: FeedLoadingView {
+    func display(isLoading: Bool) {
+        object?.display(isLoading: isLoading)
+    }
+}
 
 private final class FeedViewAdapter: FeedView {
     private weak var controller: FeedViewController?
