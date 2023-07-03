19c6c0d0414966ad1ea4fb83767365468f682db7
jmtome <jtome@fi.uba.ar>
2023-07-03 15:22:00 -0300

    Move ErrorView to separate file

---
+++ FeedFrameworkiOS/Feed UI/Views/ErrorView.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorView.swift
+//  FeedFrameworkiOS
+//
+//  Created by macbook on 03/07/2023.
+//
+
+import UIKit
+
+public final class ErrorView: UIView {
+    public var message: String?
+}
