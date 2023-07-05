fa735d464bcabfe00ccd07c18bda6b0ff6657929
jmtome <jtome@fi.uba.ar>
2023-07-04 23:51:52 -0300

    Make the LocalFeedLoader implement the FeedCache protocol (so it can be composed)

--- FeedFramework/Feed Cache/LocalFeedLoader.swift
+++ FeedFramework/Feed Cache/LocalFeedLoader.swift
@@ -75,8 +75,8 @@ public final class LocalFeedLoader {
         self.currentDate = currentDate
     }
 }
-extension LocalFeedLoader {
-    public typealias SaveResult = Result<Void, Error>
+extension LocalFeedLoader: FeedCache {
+    public typealias SaveResult = FeedCache.Result
     
     public func save(_ feed: [FeedImage], completion: @escaping (SaveResult) -> Void) {
         store.deleteCachedFeed { [weak self] deletionResult in
