534953cae7bb4bef54a5034a2f972cde96aaa1cc
jmtome <jtome@fi.uba.ar>
2023-07-05 18:58:54 -0300

    Remove unused debugging code that was previously used during UI tests

--- FeedApp/FeedApp/AppDelegate.swift
+++ FeedApp/FeedApp/AppDelegate.swift
@@ -10,8 +10,6 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
@@ -20,13 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        let configuration = UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-        
-        #if DEBUG
-        configuration.delegateClass = DebuggingSceneDelegate.self
-        #endif
-        
-        return configuration
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
