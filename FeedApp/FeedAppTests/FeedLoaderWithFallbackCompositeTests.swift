9055eaf86b1e08c5e5f135c1c16caa3fc4137c88
jmtome <jtome@fi.uba.ar>
2023-07-04 21:30:40 -0300

    Extract duplicate load logic from test into a helper method

--- FeedApp/FeedAppTests/FeedLoaderWithFallbackCompositeTests.swift
+++ FeedApp/FeedAppTests/FeedLoaderWithFallbackCompositeTests.swift
@@ -30,46 +30,21 @@ class FeedLoaderWithFallbackComposite: FeedLoader {
     }
 }
 
-
 class FeedLoaderWithFallbackCompositeTests: XCTestCase {
     
-    func test_load_deliversPrimaryFeedOnPrimaryLoadSuccess() {
+    func test_load_deliversPrimaryFeedOnPrimaryLoaderSuccess() {
         let primaryFeed = uniqueFeed()
         let fallbackFeed = uniqueFeed()
         let sut = makeSUT(primaryResult: .success(primaryFeed), fallbackResult: .success(fallbackFeed))
-        
-        let exp = expectation(description: "Wait for load completion")
-        sut.load { result in
-            switch result {
-            case let .success(receivedFeed):
-                XCTAssertEqual(receivedFeed, primaryFeed)
-                
-            case .failure:
-                XCTFail("Expected successful load feed result, got \(result) instead")
-            }
-            
-            exp.fulfill()
-        }
-        wait(for: [exp], timeout: 1)
+
+        expect(sut, toCompleteWith: .success(primaryFeed))
     }
     
-    func test_load_deliversFallbackFeedOnPrimaryFailure() {
+    func test_load_deliversFallbackFeedOnPrimaryLoaderFailure() {
         let fallbackFeed = uniqueFeed()
         let sut = makeSUT(primaryResult: .failure(anyNSError()), fallbackResult: .success(fallbackFeed))
-        
-        let exp = expectation(description: "Wait for load completion")
-        
-        sut.load { result in
-            switch result {
-            case .success(let receivedFeed):
-                XCTAssertEqual(receivedFeed, fallbackFeed)
-                
-            case .failure:
-                XCTFail("Expected successful load feed result, got \(result) instead")
-            }
-            exp.fulfill()
-        }
-        wait(for: [exp], timeout: 1.0)
+
+        expect(sut, toCompleteWith: .success(fallbackFeed))
     }
     
     // MARK: - Helpers
@@ -83,28 +58,49 @@ class FeedLoaderWithFallbackCompositeTests: XCTestCase {
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
     }
-    
-    private func anyNSError() -> NSError {
-        return NSError(domain: "any error", code: 0)
+
+    private func expect(_ sut: FeedLoader, toCompleteWith expectedResult: FeedLoader.Result, file: StaticString = #file, line: UInt = #line) {
+        let exp = expectation(description: "Wait for load completion")
+        
+        sut.load { receivedResult in
+            switch (receivedResult, expectedResult) {
+            case let (.success(receivedFeed), .success(expectedFeed)):
+                XCTAssertEqual(receivedFeed, expectedFeed, file: file, line: line)
+                
+            case (.failure, .failure):
+                break
+                
+            default:
+                XCTFail("Expected \(expectedResult), got \(receivedResult) instead", file: file, line: line)
+            }
+            
+            exp.fulfill()
+        }
+                
+        wait(for: [exp], timeout: 1.0)
     }
-    
+
     private func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #file, line: UInt = #line) {
         addTeardownBlock { [weak instance] in
             XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak.", file: file, line: line)
         }
     }
     
+    private func anyNSError() -> NSError {
+        return NSError(domain: "any error", code: 0)
+    }
+
     private func uniqueFeed() -> [FeedImage] {
         return [FeedImage(id: UUID(), description: "any", location: "any", url: URL(string: "http://any-url.com")!)]
     }
-    
+
     private class LoaderStub: FeedLoader {
         private let result: FeedLoader.Result
         
         init(result: FeedLoader.Result) {
             self.result = result
         }
-        
+
         func load(completion: @escaping (FeedLoader.Result) -> Void) {
             completion(result)
         }
