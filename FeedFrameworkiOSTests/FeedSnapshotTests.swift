8dbeafaeacf36e7d7ed56e2443523cd76e1ef12e
jmtome <jtome@fi.uba.ar>
2023-07-06 12:59:25 -0300

    Record feed with failed image loading snapshot

--- FeedFrameworkiOSTests/FeedSnapshotTests.swift
+++ FeedFrameworkiOSTests/FeedSnapshotTests.swift
@@ -35,6 +35,14 @@ class FeedSnapshotTests: XCTestCase {
         record(snapshot: sut.snapshot(), named: "FEED_WITH_ERROR_MESSAGE")
     }
     
+    func test_feedWithFailedImageLoading() {
+        let sut = makeSUT()
+        
+        sut.display(feedWithFailedImageLoading())
+        
+        record(snapshot: sut.snapshot(), named: "FEED_WITH_FAILED_IMAGE_LOADING")
+    }
+    
     // MARK: - Helpers
     private func makeSUT() -> FeedViewController {
         let bundle = Bundle(for: FeedViewController.self)
@@ -44,6 +52,23 @@ class FeedSnapshotTests: XCTestCase {
         controller.loadViewIfNeeded()
         return controller
     }
+    
+    private func feedWithFailedImageLoading() -> [ImageStub] {
+        return [
+            ImageStub(
+                description: nil,
+                location: "Cannon Street, London",
+                image: nil
+            ),
+            ImageStub(
+                description: nil,
+                location: "Brighton Seafront",
+                image: nil
+            )
+        ]
+    }
+    
+    
     private func feedWithContent() -> [ImageStub] {
         return [
             ImageStub(
