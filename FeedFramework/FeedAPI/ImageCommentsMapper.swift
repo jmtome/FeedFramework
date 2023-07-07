b082c259ebf15c54b88811588c70eceebc28e759
jmtome <jtome@fi.uba.ar>
2023-07-07 15:30:25 -0300

    Delivers proper results on 2xx response

--- FeedFramework/FeedAPI/ImageCommentsMapper.swift
+++ FeedFramework/FeedAPI/ImageCommentsMapper.swift
@@ -13,7 +13,7 @@ public final class ImageCommentsMapper {
     }
     
     static func map(_ data: Data, from response: HTTPURLResponse) throws -> [RemoteFeedItem] {
-        guard response.isOK, let root = try? JSONDecoder().decode(Root.self, from: data) else {
+        guard isOK(response), let root = try? JSONDecoder().decode(Root.self, from: data) else {
             throw RemoteImageCommentsLoader.Error.invalidData
         }
         
@@ -28,4 +28,9 @@ public final class ImageCommentsMapper {
             throw error
         }
     }
+    
+    private static func isOK(_ response: HTTPURLResponse) -> Bool {
+        (200...299).contains(response.statusCode)
+    }
 }
+
