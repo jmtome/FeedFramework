32ca4d20c68ca62f9c9822fe988ea0d68088b8b8
jmtome <jtome@fi.uba.ar>
2023-07-24 17:14:58 -0300

    Group protocol conformance in extensions

--- FeedApp/FeedApp/NullStore.swift
+++ FeedApp/FeedApp/NullStore.swift
@@ -8,19 +8,23 @@
 import Foundation
 import FeedFramework
 
-class NullStore: FeedStore & FeedImageDataStore {
+class NullStore {}
+
+extension NullStore: FeedStore {
     func deleteCachedFeed(completion: @escaping DeletionCompletion) {
         completion(.success(()))
     }
     
-    func insert(_ feed: [FeedFramework.LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+    func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
         completion(.success(()))
     }
     
     func retrieve(completion: @escaping RetrievalCompletion) {
         completion(.success(.none))
     }
-    
+}
+
+extension NullStore: FeedImageDataStore {
     func insert(_ data: Data, for url: URL, completion: @escaping (InsertionResult) -> Void) {
         completion(.success(()))
     }
